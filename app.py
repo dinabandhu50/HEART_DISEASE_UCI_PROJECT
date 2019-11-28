f2704be32b0ebf2ffe8600f785637471b1e177b9
Dinabandhu <beheradinabandhu50@gmail.com>
2019-11-28 12:36:33 +0530

    updated app.py and notebook , created models request and requirement.txt

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, jsonify
+import pickle
 app = Flask(__name__)
 
 
-@app.route('/')
-def prediction():
+@app.route('/', methods=['GET', 'POST'])
+def front_end():
     return render_template('index.html')
 
 
+@app.route('/notebook', methods=['POST'])
+def prediction():
+    data = request.get_json()
+    pred = model_saved.predict(data)
+    return jsonify(str(pred))
+
+
 if __name__ == '__main__':
-    app.run()
+    MODEL_PATH = './models/model_saved.pkl'
+    with open(MODEL_PATH, 'rb') as fid:
+        model_saved = pickle.load(fid)
+    app.run(debug=True)
